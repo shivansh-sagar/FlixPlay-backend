faf3830e627cdeea121c3c7f5f71aebd88f971f3
shivansh-sagar <shivanshsagar987@gmail.com>
2024-02-09 11:52:41 +0530

    Connect to MongoAtles

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
-import mongoose from "mongoose";
-import {DB_NAME} from "./constants";
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB();
 
 
 //FIRST APPROACH
-import express  from "express";
-const app = express();
+// import express  from "express";
+// const app = express();
 
-( async ()=>{
-    try {
-       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-       app.on("errror",(error)=>{
-        console.log("Error: Our app is not able to talk to database", error);
-        throw error
-       })
+// ( async ()=>{
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//        app.on("errror",(error)=>{
+//         console.log("Error: Our app is not able to talk to database", error);
+//         throw error
+//        })
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`App is listening on port ${process.env.PORT}`)
-})
+// app.listen(process.env.PORT, ()=>{
+//     console.log(`App is listening on port ${process.env.PORT}`)
+// })
 
-    } catch (error) {
-        console.error("ERROR:", error);
-        throw error
-    }
-})()
+//     } catch (error) {
+//         console.error("ERROR:", error);
+//         throw error
+//     }
+// })()
